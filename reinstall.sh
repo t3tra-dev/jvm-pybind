810971928cbfcdd22432f6c67294d206f4db11bc
t3tra <t3tra-dev@users.noreply.github.com>
2026-08-14 19:57:12 +0900

    version bump to v0.1.2

--- reinstall.sh
+++ reinstall.sh
@@ -3,5 +3,5 @@ deactivate || true
 rm -rf .venv
 uv sync
 uv build
-uv pip install dist/jvm_pybind-0.1.1-py3-none-any.whl
+uv pip install dist/jvm_pybind-0.1.2-py3-none-any.whl
 source .venv/bin/activate
